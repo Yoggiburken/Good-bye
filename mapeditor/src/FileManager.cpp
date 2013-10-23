d574cd60a558fe9fe837d104d4192f102bb2a6e5
eldhom <wass95@hotmail.se>
2013-10-23 12:52:04 +0200

    made a sorting algorithm for the filemanager

--- mapeditor/src/FileManager.cpp
+++ mapeditor/src/FileManager.cpp
@@ -1,4 +1,5 @@
 #include"../include/FileManager.hpp"
+#include<deque>
 
 using namespace sf;
 using namespace std;
@@ -29,9 +30,82 @@ void FileManager::writeToFile(string filename, vector<Tile>& tiles)
 
 void FileManager::sort(vector<Tile>& tiles)
 {
-	vector<vector<Sprite> > temp;
-
-	for(int i=0; i<tiles.size(); i++) {
-		
+	deque<deque<Tile>	>	temp;
+	temp.push_back(deque<Tile>(1, tiles[0]));
+	for(int i=1; i<tiles.size(); i++) {
+		Vector2f position = tiles[i].getPosition();
+		if(position.y < temp[0][0].getPosition().y) {
+			temp.push_front(deque<Tile>(1, tiles[i]));
+		} else if(position.y > temp[temp.size()-1][0].getPosition().y) {
+			temp.push_back(deque<Tile> (1, tiles[i]));
+		} else {
+			float last_y, current_y;
+			for(int j=0; j<temp.size(); j++) {
+				last_y 	= current_y;
+				current_y = temp[j][0].getPosition().y;
+				if(position.y == temp[j][0].getPosition().y) {
+					if(position.x < temp[j][0].getPosition().x || temp[j].size() == 0) {
+						temp[j].push_front(tiles[i]);
+					} else if(position.x > temp[j][temp[j].size()-1].getPosition().x) {
+						temp[j].push_back(tiles[i]);
+					} else {
+						float last_x = temp[j][0].getPosition().y, current_x;
+						for(int k=1; k<temp[j].size(); k++) {
+							current_x	= temp[j][k].getPosition().x;
+							if(position.x > last_x && position.x < current_x && k!=0) {
+								temp[j].insert(temp[j].begin()+k, tiles[i]);
+							}
+							last_x 		= current_x;
+						}
+					}
+				} else if(position.y > last_y && position.y < current_y && j!=0) { //Check if y coord doesn't exist
+					temp.insert(temp.begin()+j, deque<Tile> (1, tiles[i]));
+				}
+			}
+		}
+	}
+	
+	tiles.clear();
+	for(int i=0; i<temp.size(); i++) {
+		for(int j=0; j<temp[i].size(); j++) {
+			tiles.push_back(temp[i][j]);
+		}
 	}
 }
+
+/*
+void FileManager::sort(vector<Tile>& tiles)
+{
+	deque<deque<Tile>	>	temp;
+	for(int i=0; i<tiles.size(); i++) {
+		Vector2f position = tiles[i].getPosition();
+		if(position.y < temp[0][0].getPosition().y) {
+			temp.push_front(deque<Tile> (1, tiles[i]));
+		} else if(position.y > temp[temp.size()-1][0].getPosition().y) {
+			temp.push_back(deque<Tile> (1, tiles[i]));
+		} else {
+			float last_y, current_y;
+			for(int j=0; j<temp.size(); j++) {
+				last_y 	= current_y;
+				current_y = temp[j][0].getPosition().y;
+				if(position.y == temp[j][0].getPosition().y) {
+					if(position.x < temp[j][0].getPosition().x || temp[j].size() == 0) {
+						temp[j].push_front(tiles[i]);
+					} else if(position.x > temp[j][temp[j].size()-1].getPosition().x) {
+						temp[j].push_back(tiles[i]);
+					} else {
+						float last_x = temp[j][0].getPosition().y, current_x;
+						for(int k=1; k<temp[j].size(); k++) {
+							current_x	= temp[j][k].getPosition().x;
+							if(position.x > last_x && position.x < current_x) {
+								last_x 		= current_x;
+							}
+						}
+					}
+				} else if(position.y > last_y && position.y < current_y && j!=0) { //Check if y coord doesn't exist
+					temp.insert(temp.begin()+j-1, deque<Tile> (1, tiles[i]));
+				}
+			}
+		}
+	}
+}*/
