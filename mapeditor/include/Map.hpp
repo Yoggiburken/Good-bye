9588211e6809cbbc9d6c6b20868d87d085b27306
eldhom <wass95@hotmail.se>
2013-10-25 13:57:02 +0200

    l

--- mapeditor/include/Map.hpp
+++ mapeditor/include/Map.hpp
@@ -10,11 +10,11 @@ private:
 	std::vector<std::vector<Tile> > 	layers;
 	virtual void 						draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
-	void								replaceTile();
+	void								replaceTile(int layer_index, int tile_index, Tile& tile);
 public:
 										Map() 	{layers.reserve(4);}
 										~Map() 	{layers.clear();}
-	void								addTileToLayer(Tile& tile, int layer_index);
+	void								addTileToLayer(int layer_index, Tile& tile);
 	void								removeTileFromLayer(int layer_index, int tile_index);
 	void								clearLayer(int index);
 	void								clearAllLayers();
---
+++ mapeditor/src/Map.cpp
@@ -0,0 +1,56 @@
+#include"../include/Map.hpp"
+
+using namespace sf;
+
+
+void Map::replaceTile(int layer_index, int tile_index, Tile& tile)
+{
+	this->layers[layer_index].erase(this->layers[layer_index].begin() + tile_index);
+	this->layers[layer_index].push_back(tile);
+
+	return;
+}
+
+void Map::addTileToLayer(int layer_index, Tile& tile)
+{
+	Vector2f position = tile.getPosition();
+	for(int i=0; i<this->layers[layer_index].size(); i++) {
+		if(position == this->layers[layer_index][i].getPosition()) {
+			this->replaceTile(layer_index, i, tile);
+			return;
+		}
+	}
+	this->layers[layer_index].push_back(tile);
+}
+
+bool Map::removeTileFromLayer(int layer_index, Vector2f position)
+{
+	for(int i=0; i<this->layers[layer_index].size(); i++) {
+		if(posiiton == this->layers[layer_index][i].getPosition()) {
+			this->layers[current_layer].erase(this->layers[layer_index].begin() + i);
+			return true;
+		}
+	}
+	return false;
+}
+
+void Map::clearLayer(int layer_index)
+{
+	this->layers[layer_index].clear();
+}
+
+void Map::clearAllLayers()
+{
+	this->layers.clear();
+	this->layers.reserve(4);
+}
+
+std::vector<Tile>& Map::getLayer(int layer_index)
+{
+	return this->layers[layer_index];
+}
+
+std::vector<std::vector<Tile> >& Map::getAllLayers()
+{
+	return this->layers;
+}	
